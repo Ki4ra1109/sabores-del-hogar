dc4a062d030ed85e7a071166988c59ea757877bd
Kiara Rubio <ki.rubio@duocuc.cl>
2025-08-15 20:21:31 -0400

    feat: implementación de rutas y de archivos home y catalogo de momento vacios

---
+++ frontend/src/views/Catalogo/Catalogo.jsx
@@ -0,0 +1,10 @@
+import './Catalogo.css';
+
+export default function Catalogo() {
+  return (
+    <div className="catalogo-container">
+      <h1>Catálogo</h1>
+      <p>Próximamente...</p>
+    </div>
+  );
+}
