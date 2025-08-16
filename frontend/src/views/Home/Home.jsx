65e5dfa35719eef5bcebd2463b24eae3977fd315
Agustin <agu.liberona@duocuc.cl>
2025-08-16 02:42:39 -0400

    creacion del header en mi rama no subir a la rama main

--- frontend/src/views/Home/Home.jsx
+++ frontend/src/views/Home/Home.jsx
@@ -3,6 +3,25 @@ import './Home.css';
 export default function Home() {
   return (
     <div className="home-container">
+      <header>
+        <nav className="FondoHeader">
+          <img src='../../../public/logoFondoBlanco.svg'></img>
+          <h1 className='NombreEmpresa'>Sabores del hogar</h1>
+          
+          <div className="buscar-container">
+            <input
+              type="text"
+              placeholder="Buscar..."
+              className="buscar-input"
+            />
+            <span className="buscar-icon">🔍</span>
+          </div>
+
+        </nav>
+        <navbar>
+
+        </navbar>
+      </header>
       <h1>¡Bienvenido a Sabores de Hogar!</h1>
       <p>Repostería casera con amor 💖</p>
     </div>
