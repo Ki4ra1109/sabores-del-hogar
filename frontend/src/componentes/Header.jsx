30b839d565f3f8dcd325dd63190e5859406796e2
Agustin <agu.liberona@duocuc.cl>
2025-08-19 12:10:36 -0400

    correcion de errores generales

--- frontend/src/componentes/Header.jsx
+++ frontend/src/componentes/Header.jsx
@@ -48,7 +48,7 @@ export const Header = () => {
             <navbar className='navbar'>
                 <ul className="navbar-list">
                     <li><a href="/">Inicio</a></li>
-                    <li><a href="/productos">Productos</a></li>
+                    <li><a href="/Catalogo">Catalogo</a></li>
                     <li><a href="/nosotros">Nosotros</a></li>
                     <li><a href="/contacto">Contacto</a></li>
                     <li><a href='/'>link extra</a></li>
