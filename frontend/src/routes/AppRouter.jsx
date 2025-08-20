546054b969a960adf36d33093ccaa88dfc64fbd9
Agustin <agu.liberona@duocuc.cl>
2025-08-20 17:59:41 -0400

    vistas users listas y correcion de ortografias menores y visualizaciones

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../views/Home/Home";
 import Catalogo from "../views/Catalogo/Catalogo";
 import Login from "../views/Login/Login";
+import UserNormal from "../views/Users/Normal/UserNormal";
+import UserAdmin from "../views/Users/Admin/UserAdmin";
 
 export default function AppRouter() {
   return (
@@ -11,6 +13,8 @@ export default function AppRouter() {
           <Route path="/" element={<Home />} />
           <Route path="/Catalogo" element={<Catalogo />} />
           <Route path="/Login" element={<Login />} />
+          <Route path="/UserNormal" element={<UserNormal />}/>
+          <Route path="/UserAdmin" element={<UserAdmin />}/>
         </Routes>
       </main>
     </Router>
