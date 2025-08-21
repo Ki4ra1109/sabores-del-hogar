183e70fa746ec0c8aee95a62f2c01e567f4ab6a9
Cristobal100 <129971310+Cristobal100@users.noreply.github.com>
2025-08-21 18:09:32 -0400

    Se agrego funcion del buscador, ProductoDetalle y del menu de tortas y dulces

--- frontend/src/data/productos.js
+++ frontend/src/data/productos.js
@@ -1,12 +1,11 @@
 const productos = [
-  { id: 1, nombre: "Torta de Chocolate", precio: "25.000 - $75.000", imagen: "https://images.unsplash.com/photo-1693040743049-71cd4630ab5a?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
-  { id: 2, nombre: "Torta de Mora", precio: "20.000 - $65.000", imagen: "https://images.unsplash.com/photo-1559620192-032c4bc4674e?q=80&w=729&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
-  { id: 3, nombre: "Torta de Frutilla", precio: "22.000 - $70.000", imagen: "https://images.unsplash.com/photo-1579356094148-9b74dab60f5b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
-  { id: 4, nombre: "Torta de Zanahoria", precio: "18.000 - $60.000", imagen: "https://plus.unsplash.com/premium_photo-1714669899928-eb0b28430295?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
-  { id: 5, nombre: "Torta de Red Velvet", precio: "19.500 - $56.000", imagen: "https://plus.unsplash.com/premium_photo-1713920189849-61a19937fbda?q=80&w=688&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
-  { id: 6, nombre: "Torta de Limon", precio: "18.500 - $40.000", imagen: "https://images.unsplash.com/photo-1728911296471-8c57f645a44e?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
-  { id: 7, nombre: "Torta de Piña", precio: "13.500 - $30.000", imagen: "https://images.unsplash.com/photo-1633062781822-e32867fe7d4a?q=80&w=1166&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
-  { id: 8, nombre: "Torta de Frambuesa", precio: "18.500 - $35.000", imagen: "https://plus.unsplash.com/premium_photo-1714670623261-a2e9797d4e05?q=80&w=688&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+  { id: 1, nombre: "Torta de Chocolate", categoria: "tortas", precio: "25.000 - $75.000", imagen: "https://..." },
+  { id: 2, nombre: "Torta de Mora",       categoria: "tortas", precio: "20.000 - $65.000", imagen: "https://..." },
+  { id: 3, nombre: "Torta de Frutilla",    categoria: "tortas", precio: "22.000 - $70.000", imagen: "https://..." },
+  { id: 4, nombre: "Torta de Zanahoria",   categoria: "tortas", precio: "18.000 - $60.000", imagen: "https://..." },
+  { id: 5, nombre: "Torta de Red Velvet",  categoria: "tortas", precio: "19.500 - $56.000", imagen: "https://..." },
+  { id: 6, nombre: "Torta de Limon",       categoria: "tortas", precio: "18.500 - $65.000", imagen: "https://..." },
+  { id: 7, nombre: "Torta de Piña",        categoria: "tortas", precio: "13.500 - $70.000", imagen: "https://..." },
+  { id: 8, nombre: "Torta de Frambuesa",   categoria: "tortas", precio: "18.500 - $75.000", imagen: "https://..." },
 ];
-
-export default productos;
+export default productos;;
