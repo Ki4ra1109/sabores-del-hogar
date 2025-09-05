c83f393050566f099792c78f718e036c7da4a423
Cristobal100 <129971310+Cristobal100@users.noreply.github.com>
2025-09-05 02:12:24 -0400

    cupon de descuento funcional

---
+++ backend/Routes/cuponRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const cuponCtrl = require("../controllers/cuponController");
+
+router.get("/", cuponCtrl.list);
+router.get("/:id", cuponCtrl.getOne);
+router.post("/", cuponCtrl.create);
+router.put("/:id", cuponCtrl.update);
+router.delete("/:id", cuponCtrl.remove);
+
+router.post("/validate", cuponCtrl.validateForOrder);
+
+module.exports = router;
